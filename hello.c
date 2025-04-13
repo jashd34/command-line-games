99f45aee7532ebe248b72f2d8ee0ee52ffb02d0f
Sebi <davidhamani@outlook.com>
2025-04-13 16:35:35 +0100

    hello.c pause

--- hello.c
+++ hello.c
@@ -2,5 +2,8 @@
 
 int main() {
     printf("Hello, World!\n");
+    // pause for user input
+    printf("Press Enter to continue...\n");
+    getchar(); // Wait for user to press Enter
     return 0;
 }
