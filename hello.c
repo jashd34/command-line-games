b9e6cfc817b31cf34a0700c8f77e51bbdffd7b64
Sebi <davidhamani@outlook.com>
2025-04-13 17:05:50 +0100

    scan

--- hello.c
+++ hello.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age); 
+
+    printf("My name is %s and my age is %d\n", name, age);
+
+    float pi = 3.14f;
+    printf("The value of pi is approximately %.2f\n", pi);
     // pause for user input
     printf("Press Enter to continue...\n");
     getchar(); // Wait for user to press Enter
     return 0;
 }
+
+// gcc hello.c -o hello.exe && start hello.exe
